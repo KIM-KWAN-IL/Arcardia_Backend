8543ac544887b32109b3c40d325478f6d46bec81
KIM-KWAN-IL <118953934+KIM-KWAN-IL@users.noreply.github.com>
2024-05-20 01:52:45 +0900

    Add entity

---
+++ src/main/java/profit/login/entity/User.java
@@ -0,0 +1,125 @@
+package profit.login.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@Table(name = "users")
+@Entity
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private String id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(unique = true, length = 100, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;      // 권한
+
+//    @OneToMany(mappedBy = "user", orphanRemoval = true)
+//    private List<Board> boards;     // 작성글
+
+    @OneToMany(mappedBy = "user", orphanRemoval = true)
+    private List<Like> likes;       // 유저가 누른 좋아요
+
+    @OneToMany(mappedBy = "user", orphanRemoval = true)
+    private List<Comment> comments; // 댓글
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+
+    private Integer receivedLikeCnt; // 유저가 받은 좋아요 개수(본인제외)
+    public void likeChange(Integer receivedLikeCnt) {
+        this.receivedLikeCnt = receivedLikeCnt;
+
+        //우리는 아래형식을 좋아요수 대신 포인트로 대체예정
+        if (this.receivedLikeCnt >= 10 && this.userRole.equals(UserRole.SILVER)) {
+            this.userRole = UserRole.GOLD;
+        }
+    }
+
+
+    private String nickname;    // 닉네임
+    public void edit(String newPassword, String newNickname) {
+        this.password = newPassword;
+        this.nickname = newNickname;
+    }
+
+    // admin 페이지 만들거면 아래 등급을 일반과 전문가로 이분화 하여서 진행해야함
+    //일단 예시로 만들어놓음.
+    public void changeRole() {
+        if (userRole.equals(UserRole.BRONZE)) userRole = UserRole.SILVER;
+        else if (userRole.equals(UserRole.SILVER)) userRole = UserRole.GOLD;
+        else if (userRole.equals(UserRole.GOLD)) userRole = UserRole.BLACKLIST;
+        else if (userRole.equals(UserRole.BLACKLIST)) userRole = UserRole.BRONZE;
+    }
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+
+    //복호화해서 가져오는 비밀번호
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+
+    // Getters and setters
+}
