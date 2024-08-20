8d5a87726f08f57d9fc0a6e1d823acc34387b1da
KIM-KWAN-IL <david5452@NAVER.COM>
2024-08-20 10:21:51 +0900

    답글 쓰기, 읽기, 에러 반환 DTO 생성

---
+++ src/main/java/profit/login/question_board/response/CommentReadResponse.java
@@ -0,0 +1,17 @@
+    package profit.login.question_board.response;
+
+
+    import lombok.Builder;
+    import lombok.Data;
+    import profit.login.question_board.Entity.Comment;
+
+    import java.util.List;
+
+    @Data
+    @Builder
+    public class CommentReadResponse {
+        private String message;
+        private List<Long> userId;
+        private String nickName;
+        private List<Comment> comments;
+    }
